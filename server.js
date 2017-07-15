4a9d72bf8e661fb47861561a9c3e3585d823e478
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-14 15:59:27 -1000

    set data string, split to array, saved to variable

--- server.js
+++ server.js
@@ -3,9 +3,25 @@
 const net = require('net');
 
 const server = net.createServer((request) => {
+var clientRequest;
+
+///header
+var Host = "Host: www.alpacasrule.com";
+var Connection = "Connection: Keep-Alive";
+var Accept = "Accept: text/html, application/json";
+var Today = new Date();
+
+//body includes Host, Accept
+var bodyConnection = "Connection:Keep-Alive";
+var Content_Length = "Content_Length: 278";
 
   request.on('data', (data) => {
-    console.log(data);
+    clientRequest = data.toString().split(' ', 3);
+
+    if(!clientRequest.includes('HTTP')){
+      console.log("moo!");
+      request.write(Host);
+    }
     request.end();
   });
 

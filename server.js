b96c717c81732fdf01fee94d37afaa15efd1a80c
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-15 19:54:15 -1000

    both curls work! need to work on handling 404

--- server.js
+++ server.js
@@ -5,69 +5,54 @@ const fs = require('fs');
 const path = require('path');
 
 const server = net.createServer((request) => {
-var clientRequest;
 
-///header
 var statusLine = "HTTP/1.1 200 OK";
 var errorStatusLine = "HTTP/1.0 404 Not Found";
 var myServer = "Server: Alpaca Land!";
 var Today = "Date: " + new Date();
 var Content_Type = "Content-Type: text/html; charset=utf-8";
 var Connection = "Connection: Keep-Alive";
-//body includes Host, Accept
-var bodyConnection = "test";
-var Content_Length = "test";
 
 var present = statusLine + '\n'+ myServer + '\n'+ Today + '\n' + Content_Type + '\n'+ Connection;
 
-  request.on('data', (data) => {
-    clientRequest = data.toString().split(' ', 3);
+    request.on('data', (data) => {
+      clientRequest = data.toString().split(' ', 3);
 
-    if(!clientRequest.includes('HTTP')){
-        urlRequest = clientRequest[1];
-
-        switch(urlRequest ){
-          case '/':
-
+        method = clientRequest[0];
+        switch(method){
+          case 'GET':
+                 processGet(clientRequest[1]);
             break;
-          case '/index.html':
-                 transform(urlRequest);
-
+          case 'HEAD':
+                 processHead(clientRequest[1]);
             break;
-          case '/hydrogen.html':
-                 transform(urlRequest);
-
-            break;
-          case '/helium.html':
-                 transform(urlRequest);
-
-            break;
-          case '/404.html':
-                 transform(urlRequest);
-
-            break;
-          case '/css/styles.css':
-                 transform(urlRequest);
-
-            break;
-            default: request.end();
+            default: processGet('./404.html');
         }
-      }
-  });
 
-  function transform(file){
-    fs.readFile('.' + file, (err, data) => {
-      //will return contents of file
-      if(err) throw err;
-      var moo = data.toString();
-      bodyConnection = moo;
-      Content_Length = moo.length;
-      present += '\n'+ "Content-Length:" + Content_Length + '\n' + ' ' + '\n' +bodyConnection;
-      console.log(present);
-      request.write(present);
-      request.end();
     });
-  }
+
+    function processHead(file){
+      fs.readFile('.' + file, (err, data) => {
+        if(err) throw err;
+        var moo = data.toString();
+        Content_Length = moo.length;
+        present += '\n'+ "Content-Length:" + Content_Length;
+        request.write(present);
+        request.end();
+      });
+    }
+
+    function processGet(file){
+      fs.readFile('.' + file, (err, data) => {
+        if(err) throw err;
+         var moo = data.toString();
+          bodyConnection = moo;
+          Content_Length = moo.length;
+          present += '\n'+ "Content-Length:" + Content_Length + '\n\n' + bodyConnection;
+          request.write(present);
+          request.end();
+      });
+    }
 });
 
 
